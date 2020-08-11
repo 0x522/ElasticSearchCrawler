a38c43debb1ebbbee65f20907be4854fca94f118
cyunt <chenyuntao1997@163.com>
2020-08-11 13:10:05 +0800

    finishDatabaseOperation

---
+++ src/main/java/com/github/hcsp/News.java
@@ -0,0 +1,60 @@
+package com.github.hcsp;
+
+import java.util.Date;
+
+public class News {
+    private Integer id;
+    private String title;
+    private String content;
+    private String url;
+    private Date created_at;
+    private Date modified_at;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+    public Date getModified_at() {
+        return modified_at;
+    }
+
+    public void setModified_at(Date modified_at) {
+        this.modified_at = modified_at;
+    }
+}
