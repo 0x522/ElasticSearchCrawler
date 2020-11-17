0e41df074739669f7955b9e0fdc6ca41665fe949
YUNTAO <36143626+0x522@users.noreply.github.com>
2020-11-18 00:35:08 +0800

    Update Crawler.java

--- src/main/java/com/github/hcsp/Crawler.java
+++ src/main/java/com/github/hcsp/Crawler.java
@@ -1,6 +1,5 @@
 package com.github.hcsp;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
